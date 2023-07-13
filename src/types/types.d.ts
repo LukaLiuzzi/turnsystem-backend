187999130904d4f991fac1f73772298ff8b13739
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-07-13 19:14:30 -0300

    user and client interfaces

---
+++ src/types/types.d.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id: number
+  username: string
+  password: string
+  email: string
+  phone_number: string
+}
+
+export interface Client {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  phone_number: string
+}
