4ce458e82011b40dda363c50f269e8d6affd9b03
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-07-15 02:32:08 -0300

    add passport local strategy

---
+++ src/libs/passport.ts
@@ -0,0 +1,64 @@
+import passport from "passport"
+import { Strategy as LocalStrategy } from "passport-local"
+import { pool } from "../config"
+import { RowDataPacket } from "mysql2"
+import { User } from "../types/types"
+import { createHttpError } from "../helpers"
+import bcrypt from "bcrypt"
+import { Request } from "express"
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passReqToCallback: true },
+    async (req: Request, email: string, password: string, done) => {
+      try {
+        // Buscar al usuario por email en la base de datos
+        const [results] = await pool.query<RowDataPacket[]>(
+          "SELECT * FROM users WHERE email = ?",
+          [email]
+        )
+
+        if (results.length === 0) {
+          return done(createHttpError(404, "Credenciales inválidas"))
+        }
+
+        const user: User = results[0] as User
+
+        // Comparar la contraseña del usuario encontrado con la proporcionada
+        const validPassword = await bcrypt.compare(password, user.password)
+
+        if (!validPassword) {
+          return done(createHttpError(404, "Credenciales inválidas"))
+        }
+
+        // Si el usuario existe y la contraseña es válida, retornar el usuario
+        return done(null, user)
+      } catch (err) {
+        return done(err)
+      }
+    }
+  )
+)
+
+// Serializar al usuario para almacenarlo en la sesión
+passport.serializeUser((user: any, done) => {
+  done(null, user.id)
+})
+
+// Deserializar al usuario para obtener sus datos desde la sesión
+passport.deserializeUser(async (id: string, done) => {
+  const [results] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE id = ?",
+    [id]
+  )
+
+  if (results.length === 0) {
+    return done(createHttpError(404, "Usuario no encontrado"))
+  }
+
+  const user: User = results[0] as User
+
+  return done(null, user)
+})
+
+export default passport
