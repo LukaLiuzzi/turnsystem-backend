f40e43520f93ab7727b87bca7e2e39bade40aab5
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-07-15 03:36:02 -0300

    cors config

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(compression())
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+)
 app.use(helmet())
 
 // Configuración de sesión
