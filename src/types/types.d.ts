fd096c0d446b0818ab406d9da6e850d593a3c17c
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-07-15 02:33:53 -0300

    add db tables types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -13,3 +13,36 @@ export interface Client {
   email: string
   phone_number: string
 }
+
+export enum TurnStatus {
+  PENDING = "Pending",
+  CONFIRMED = "Confirmed",
+  CANCELLED = "Cancelled",
+}
+export interface Turn {
+  client_id: number
+  date: string
+  time: string
+  status: TurnStatus
+  service_id: number
+}
+
+export interface Service {
+  name: string
+  description: string
+  price: number
+}
+
+export enum PaymentStatus {
+  PENDING = "Pending",
+  PAID = "Paid",
+}
+export interface Payment {
+  turn_id: number
+  amount: number
+  status: PaymentStatus
+}
+
+export interface Administrator {
+  user_id: number
+}
