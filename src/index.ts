063137af3eb3d950ed4eb5adb448f49ebced409c
Luka <lukaliuzzidev@gmail.com>
2023-07-21 21:55:39 -0300

    export server for testing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { app } from "./app"
 import { PORT } from "./config"
+import { IncomingMessage, ServerResponse, Server } from "http"
 
+let server: Server<typeof IncomingMessage, typeof ServerResponse>
 try {
-  app.listen(PORT, () => {
+  server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
 } catch (error) {
   console.error(error)
 }
+
+export { server }
